cce77637b2d918af81048e3bad26acf2f9cd6891
ericguo1230 <49818848+ericguo1230@users.noreply.github.com>
2020-09-05 13:23:25 -0400

    Update PasswordVerify.php

--- PasswordVerify.php
+++ PasswordVerify.php
@@ -2,7 +2,7 @@
 <body>
 <?php
 // See the password_hash() example to see where this came from.
-$hash = '$2y$12$bayVp/x9PTmOSPUlvDfqYOP8HK/.eOtYFtDEjLq2036Ck8fqTLjzu';
+$hash           = '$2y$12$bayVp/x9PTmOSPUlvDfqYOP8HK/.eOtYFtDEjLq2036Ck8fqTLjzu';
 $input_password = $_POST["psw"];
 
 if (password_verify($input_password, $hash)) {
